322e0657a6aea1acb889c727ab3cc108d2a9421a
hyoyoungjung <hy8grace@gmail.com>
2023-05-04 00:11:12 +0900

    Programmers - Lv.1

--- Programmers/src/lv1_09_intSquareRoot/Solution.java
+++ Programmers/src/lv1_09_intSquareRoot/Solution.java
@@ -1,5 +1,13 @@
 package lv1_09_intSquareRoot;
 
-public class Solution {
-	
+class Solution {
+	public long solution(long n) {
+		long sqrt = (long) Math.sqrt(n);
+
+		if (sqrt * sqrt == n) {
+			return (long) Math.pow((sqrt + 1), 2);
+		} else {
+			return -1;
+		}
+	}
 }
--- Programmers/src/lv1_10_numIntoArr/Solution.java
+++ Programmers/src/lv1_10_numIntoArr/Solution.java
@@ -1,5 +1,29 @@
 package lv1_10_numIntoArr;
 
-public class Solution {
+class Solution {
+	public int[] solution(long n) {
 
+		// n의 자릿수 파악
+		int cnt = 0; // 자릿수 저장
+		long temp = n; // 몫 저장
+
+		while (temp > 0) {
+			temp /= 10;
+			cnt++;
+		}
+
+		// n의 자릿수 만큼의 길이를 가진 배열 생성
+		int[] answer = new int[cnt];
+
+		temp = n;
+		long remainder = 0;
+
+		// n의 뒷자리 부터 배열에 넣기
+		for (int i = 0; i < answer.length; i++) {
+			remainder = temp % 10;
+			temp /= 10;
+			answer[i] = (int) remainder;
+		}
+		return answer;
+	}
 }
